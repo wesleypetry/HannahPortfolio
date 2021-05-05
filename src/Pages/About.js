afef7c9c8d56b06d420947dd46c70129e704a815
wesleypetry <wesleypetry@ufl.edu>
2021-05-05 13:08:33 -0400

    Fixed style sheet formatting

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './styles.modules.css';
 
 function About(props) {
     return (
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './styles.modules.css';
 
 function Home(props) {
     return (
