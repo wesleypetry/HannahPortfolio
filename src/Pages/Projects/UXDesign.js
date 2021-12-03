afba85efd071de7164ab890bdff420c651b8c06e
hbraswell0 <83733685+hbraswell0@users.noreply.github.com>
2021-12-03 11:41:06 -0500

    UX Design page, text on home page about

---
+++ src/Pages/Projects/UXDesign.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './projects.css';
+
+function UXDesign(props) {
+    return (
+        <div classname="container">
+            <div className="colorheader">
+                <div className="headertext">
+                    <h1>User Experience Design</h1>
+                    <h3>This project is a redesign of the Bank of America mobile app. </h3>
+                </div>
+            </div>
+            <div className="casestudy">
+                <h2>The Task</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+                <h2>Inspiration + Process</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+                <h2>Finished Designs</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+                <h2>Key Takeaways</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+            </div>
+        </div>
+    );
+
+}
+export default UXDesign;
--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -3,8 +3,90 @@ import './styles.modules.css';
 
 function Work(props) {
     return (
+<<<<<<< Updated upstream
         <div className="WorkDiv">
             <h1>Hannah's Work Page</h1>
+=======
+        <div>
+            <div className="HomeDiv">
+                <div className="IntroHeaderDiv">
+                    <h1>Hi, I'm Hannah</h1>
+                </div>
+                <div className="IntroTextDiv">
+                    <div>
+                        <p>I’m an aspiring user experience designer. I’m passionate about crafting user experiences and design systems for mobile and web products. </p>
+                        <p>I am currently finishing my Bachelor’s Degree in Graphic Communications at Clemson University and am graduating in May 2022. </p>
+                    </div>
+                </div>
+            </div>
+            <div className="WorkDiv">
+                <div className="WorkGrid">
+                    <a href = "/Work">
+                        <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p>AccruePartners Internship</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/Work">
+                        <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p>Pieper O'Brien Herr Internship</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/Work">
+                    <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p></p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/Work">
+                    <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p></p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/UXDesign">
+                        <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p>User Experience Design</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/Work">
+                        <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p>Brand Creation and Design Concepts</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/Work">
+                        <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p>Clemson Ultimate Frisbee Website Concept</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/Work">
+                        <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p>Brand Creation and Design Concepts</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href = "/Work">
+                    <div className="GridItem">
+                            <div className="GridOverlay">
+                                <p></p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+>>>>>>> Stashed changes
         </div>
     );
 
