c70c8a3837978fd88f19f4bf7ea9d7395ca42a93
hbraswell0 <83733685+hbraswell0@users.noreply.github.com>
2021-12-10 14:05:16 -0500

    bank app image added, ux design page text

--- src/Pages/Projects/UXDesign.js
+++ src/Pages/Projects/UXDesign.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './projects.css';
+import bankapp from './ProjectImages/bankapp.png';
+
 
 function UXDesign(props) {
     return (
@@ -12,13 +14,14 @@ function UXDesign(props) {
             </div>
             <div className="casestudy">
                 <h2>The Task</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+                <p>During a User Experience Design course at Clemson, I was tasked with creating an app design centered around the experience of the user. I chose to redesign the Bank of America mobile app to be cleaner, more streamlined and have a better interface.</p>
                 <h2>Inspiration + Process</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+                <p>I personally am a user of the Bank of America app. It has always felt difficult to navigate and accomplish tasks on. This is why I decided to redesign it and focus on the user this time. Bank of America has already established their brand colors, so that was something that I did not change.<br/><br/>First I created a moodboard of web apps and styles that that inspired me. After, I created a clean workflow that made sense. Having a simple workflow was very important to me because I wanted to make the user’s experience with the app as easy and straight-forward as possible. The next step was to create wireframes to clarify consistent ways for displaying particular types of information on the interface. From there, I curated high-fidelity mockups in order to show more detail and give a better taste of what the real user interface elements might look like. Finally, I used Invision to prototype my design and really make it come to life.</p>
                 <h2>Finished Designs</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+                <p>After a moodboards and wireframes, I moved on to high-fidelity mockups and prototyping. Below are the final high-fidelity mockups. Click <a href="https://invis.io/FE124XZP586Q" target="_blank" rel="noreferrer noopener">here</a> to view the Invision prototype.</p>
+                <img className="brandguide" src = {bankapp} alt = "bankapp"/>
                 <h2>Key Takeaways</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet nulla. Odio tempor orci dapibus ultrices in iaculis nunc. Blandit aliquam etiam erat velit scelerisque. Eget mi proin sed libero. Quis varius quam quisque id. Morbi tristique senectus et netus et malesuada fames ac. Nec feugiat in fermentum posuere. Ac turpis egestas sed tempus urna et. Cras pulvinar mattis nunc sed blandit libero. Tincidunt ornare massa eget egestas purus viverra accumsan. </p>
+                <p>This was one of my favorite projects in my college career. I learned the ins and outs of user experience design. I think that I accomplished my goal of making the Bank of America app more refined and user-friendly. The design really draws attention to the most important reason why users open the banking app. I feel more confident in designing mobile and web apps for the user after completing this project.</p>
             </div>
         </div>
     );
