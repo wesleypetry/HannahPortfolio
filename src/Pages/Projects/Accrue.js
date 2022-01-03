7b302b2f9cdd7c56208b46af486286bdbbf4e378
hbraswell0 <83733685+hbraswell0@users.noreply.github.com>
2022-01-03 00:53:50 -0500

    project text, images

--- src/Pages/Projects/Accrue.js
+++ src/Pages/Projects/Accrue.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import './projects.css';
-import bankapp from './ProjectImages/bankapp.png';
+import AccrueImage from './ProjectImages/accrue.png';
 
 
 function Accrue(props) {
@@ -35,10 +35,10 @@ function Accrue(props) {
                 <p>I drew a lot of inspiration from the blue and orange colors in AccruePartners’ brand guidelines, the triangle “A” shapes in the logo, and the circular shape of the logo. Prior to creating any designs, I also researched ways to draw engagement from followers and decided to include things like polls and questions in my designs. 
                     <br/><br/>After brainstorming, I created a few “Consultant Announcement” posts to announce new consultants that were joining the company. Then I began creating posts for every holiday through the end of the year. Next, I created polls, recognition announcments, and other fun posts, like a summer playlist. I used Hootsuite to schedule these images for future posting and Monday.com to manage all projects with my supervisor. 
                     <br/><br/>In June, after I was comfortable curating and scheduling posts two weeks in advance, I completed a full social media audit of not only AccruePartners social media platforms but also the competitors’ platforms. This including recording follower count, post frequency, engagement rate, and more. 
-                    <br/><br/>In the midst of designing posts for social media, I also wrote a few articles for the AccrueParters Blog on the website. View one of the blogs here.</p>
+                    <br/><br/>In the midst of designing posts for social media, I also wrote a few articles for the AccrueParters Blog on the website. View one of the blogs <a href="https://accruepartners.com/redesigning-work-post-covid/" target="_blank" rel="noreferrer noopener">here</a>.</p>
                 <h2>Finished Designs</h2>
                 <p>I stuck to AccruePartners’ brand guidelines throughout my posts while also adding different elements that they had not done previously. Below you can view some of the social media posts I designed and output to each of the company’s platforms.</p>
-                <img className={brandguideStyle} onClick={changeStyle} src = {bankapp} alt = "bankapp"/>
+                <img className={brandguideStyle} onClick={changeStyle} src = {AccrueImage} alt = "accrue"/>
                 <h2>Key Takeaways</h2>
                 <p>I learned many useful skills and advice during this internship. I learned how to use Hootsuite and Monday.com. I also learned how to understand a brand’s voice quickly and effectively. Being a fully remote intern introduced me to a different kind of work environment, but it also allowed me to continue to learn more about the staffing industry while making an impact with the team at AccruePartners.</p>
             </div>
--- src/Pages/Projects/ClemsonFrisbee.js
+++ src/Pages/Projects/ClemsonFrisbee.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import './projects.css';
-import bankapp from './ProjectImages/bankapp.png';
+import frisbeesite from './ProjectImages/frisbeesite.png';
 
 
 function ClemsonFrisbee(props) {
@@ -33,11 +33,13 @@ function ClemsonFrisbee(props) {
                 <p>During my sophomore year at Clemson, I learned HTML and CSS. I was tasked with a semester-long project of designing and developing a conceptual, responsive website for a student client.</p>
                 <h2>Process</h2>
                 <p>I first met with a student-client to discuss their purpose, goals and aesthetic look that they wanted the site to accomplish. 
-                    <br/><br/>I decided to create a label for a beer bottle that showcased an octopus logo with an ocean in the background. Putting the name “Octoholic” and other alcohol information on the design really made the label look like it could be on a shelf at the grocery store. I then printed the labels on a flexography press using 4-color CMYK inks. On my final labels, I also printed a silver metallic cold foil outline on the octopus logo. This element really brought a unique element to the label and would definitely make it pop if it were on a shelf among the rest of the bottled beers. 
-                    <br/><br/>I then created a 4-pack beer carrier to accompany the beer bottle label. I blew up the octopus logo to really emphasize it on the package. I added the name and necessary information to the diecut. I digitally printed the package on a flatbed press then cut and folded it together. Having these two matching pieces together made me very proud because they looked as though they were a real product. </p>
+                    <br/><br/>This client wanted a website to showcase the Clemson Women’s Ultimate Frisbee Team. They wanted to feature the roster, practice and tournament information and a contact form. They provided images and text they wanted to include on the site. They had a few ideas sketched that we discussed and determined one of the sketches to be the most ideal. 
+                    <br/><br/>I began by creating wireframes using Adobe XD. I then previewed these wireframes with the client where we tweaked details. After agreeing on the wireframes, I moved on to high fidelity mockups to ensure the coding process would go as smoothly as possible. 
+                    <br/><br/>I then began coding the website using HTML and CSS languages. Since this was my first time coding a full site, it was a great challenge for me. I also made the site responsive with three different media screens: desktop, tablet and mobile phone. I was able to figure out how to code the site exactly like the mockups, and I was very pleased with the final website. 
+                </p>
                 <h2>Finished Designs</h2>
                 <p>I took the student client’s wants and needs into consideration as a priority when I was designing this site. After multiple meetings with the client, they were greatly pleased with the result. View the final designs below or click here to see the site on your browser.</p>
-                <img className={brandguideStyle} onClick={changeStyle} src = {bankapp} alt = "bankapp"/>
+                <img className={brandguideStyle} onClick={changeStyle} src = {frisbeesite} alt = "frisbeesite"/>
                 <h2>Key Takeaways</h2>
                 <p>Since this was my first time utilizing HTML and CSS code, I learned so much about designing websites. This was also the first time I had been introduced to the importance of the user experience of sites. This project really made me fall in love with user interface and design. Even though I may have done some things differently if this were a project I was doing currently, I found my passion through this project and am super proud of the final website that I designed and developed.</p>
             </div>
