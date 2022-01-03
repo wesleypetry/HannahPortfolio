1909c9a958c0f9dca368d7787e7795134767bbe8
wesleypetry <wesleypetry@ufl.edu>
2022-01-02 19:09:30 -0500

    Fixed loading of hero image

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -16,7 +16,6 @@ function About(props) {
                     <p>User experience and code was first introduced to me during my sophomore year at Clemson. I took a course on web development where I learned how to code HTML and CSS. Since then, I have fallen in love with web design and development. My passion for user experience and interface expanded when I took a UX design course. I was able to go through the entire process of designing a mobile app with the user in mind. During this course, I was also tasked with designing and coding my own portfolio – the one you’re looking at right now! I took it upon myself to learn ReactJs to code my portfolio as it was something that I had been wanting to learn, and it was a great way to challenge myself.</p>
                     <p>My goal is to obtain knowledge and experience with design-related industries in the real world. I have an extreme passion for user experience and interface, and I would love to pursue this passion in my career. I see myself being an important asset to a team focused on design, development and the user. </p>
                     <h2>Contact Me</h2>
-                    
                     <p>hbraswe@g.clemson.edu</p>
                  </div>
             </div>
--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -1,21 +1,17 @@
 import React from "react";
 import './styles.modules.css';
-import heroimage from './Images/heroimage.png';
 
 
 function Work(props) {
     return (
         <div>
             <div className="HomeDiv">
-                <img className="HeroImage" src = {heroimage} alt = "heroimage" />
                 <div className="IntroHeaderDiv">
                     <h1>Hi, I'm Hannah</h1>
                 </div>
                 <div className="IntroTextDiv">
-                    
                         <p>I’m an aspiring user experience designer. I’m passionate about crafting user experiences and design systems for mobile and web products. </p>
                         <p>I am currently finishing my Bachelor’s Degree in Graphic Communications at Clemson University and am graduating in May 2022. </p>
-                    
                 </div>
             </div>
             <div className="WorkDiv">
